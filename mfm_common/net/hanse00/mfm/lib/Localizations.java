a98c8b8b258c9713d245abbc775e2bd469d69f34
Philip Hansen <philmeister1994@gmail.com>
2013-06-02 15:20:41 +0200

    Added danish localization

--- mfm_common/net/hanse00/mfm/lib/Localizations.java
+++ mfm_common/net/hanse00/mfm/lib/Localizations.java
@@ -13,6 +13,6 @@ public class Localizations {
 
     private static final String LANG_RESOURCE_LOCATION = "/mods/MFM/lang/";
 
-    public static String[] localeFiles = {LANG_RESOURCE_LOCATION + "en_US.xml"};
+    public static String[] localeFiles = {LANG_RESOURCE_LOCATION + "en_US.xml", LANG_RESOURCE_LOCATION + "da_DK.xml"};
 
 }
